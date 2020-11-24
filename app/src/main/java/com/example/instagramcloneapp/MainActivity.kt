2067fc90956ec5e66948955975a406a773c2210f
math-geec <victordean087@gmail.com>
2020-11-24 21:58:05 +0000

    connect bottom navigation bar to fragments

--- app/src/main/java/com/example/instagramcloneapp/MainActivity.kt
+++ app/src/main/java/com/example/instagramcloneapp/MainActivity.kt
@@ -18,20 +18,22 @@ import com.example.instagramcloneapp.Fragments.SearchFragment
 class MainActivity : AppCompatActivity() {
 
 //    private lateinit var textView: TextView
-    internal var selectedFragment: Fragment? = null
+//    internal var selectedFragment: Fragment? = null
 
     private val onNavigationItemSelectedListener =
         BottomNavigationView.OnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.nav_home -> {
                    // textView.setText("Home")
-                    selectedFragment = HomeFragment()
-//                    return@OnNavigationItemSelectedListener true
+                   // selectedFragment = HomeFragment()
+                    moveToFragment(HomeFragment())
+                   return@OnNavigationItemSelectedListener true
                 }
                 R.id.nav_search -> {
 //                    textView.setText("Search")
-                    selectedFragment = SearchFragment()
-//                    return@OnNavigationItemSelectedListener true
+//                     selectedFragment = SearchFragment()
+                    moveToFragment(SearchFragment())
+                   return@OnNavigationItemSelectedListener true
                 }
                 R.id.nav_add_post -> {
 //                    textView.setText("Add Post")
@@ -39,22 +41,24 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.nav_notifications -> {
 //                    textView.setText("Notifications")
-                    selectedFragment = NotificationsFragment()
-//                    return@OnNavigationItemSelectedListener true
+//                     selectedFragment = NotificationsFragment()
+                    moveToFragment(NotificationsFragment())
+                   return@OnNavigationItemSelectedListener true
                 }
                 R.id.nav_profile -> {
 //                    textView.setText("Profile")
-                    selectedFragment = ProfileFragment()
-//                    return@OnNavigationItemSelectedListener true
+//                     selectedFragment = ProfileFragment()
+                    moveToFragment(ProfileFragment())
+                   return@OnNavigationItemSelectedListener true
                 }
             }
 
-            if(selectedFragment != null){
-                supportFragmentManager.beginTransaction().replace(
-                    R.id.fragment_container,
-                    selectedFragment!!
-                ).commit()
-            }
+            // if(selectedFragment != null){
+            //     supportFragmentManager.beginTransaction().replace(
+            //         R.id.fragment_container,
+            //         selectedFragment!!
+            //     ).commit()
+            // }
             false
         }
 
@@ -66,10 +70,18 @@ class MainActivity : AppCompatActivity() {
 //        textView = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
-        supportFragmentManager.beginTransaction().replace(
-            R.id.fragment_container,
-            HomeFragment()
-        ).commit()
+        // supportFragmentManager.beginTransaction().replace(
+        //     R.id.fragment_container,
+        //     HomeFragment()
+        // ).commit()
 
+        moveToFragment(HomeFragment())
+
+    }
+
+    private fun moveToFragment(fragment: Fragment) {
+        val fragmentTrans = supportFragmentManager.beginTransaction()
+        fragmentTrans.replace(R.id.fragment_container, fragment)
+        fragmentTrans.commit()
     }
 }
