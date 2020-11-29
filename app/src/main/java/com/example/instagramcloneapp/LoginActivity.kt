5d329f4966c68b54d41d79c62e63fb51b0d391af
math-geec <victordean087@gmail.com>
2020-11-29 23:27:45 +0000

    set up login and signup screen

---
+++ app/src/main/java/com/example/instagramcloneapp/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.example.instagramcloneapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        signup_link.setOnClickListener {
+            startActivity(Intent(this, SignupActivity::class.java))
+        }
+    }
+}
