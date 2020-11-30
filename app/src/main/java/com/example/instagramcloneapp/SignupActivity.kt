1acf6eb5783503a2cf9ba04474aae5092487af56
math-geec <victordean087@gmail.com>
2020-11-30 09:54:54 +0000

    set login screen to be default start page

--- app/src/main/java/com/example/instagramcloneapp/SignupActivity.kt
+++ app/src/main/java/com/example/instagramcloneapp/SignupActivity.kt
@@ -1,11 +1,18 @@
 package com.example.instagramcloneapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        signup_btn.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
